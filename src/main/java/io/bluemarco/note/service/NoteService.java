b763e101f2cc64148575134a51a1edd74df326f0
bluemarco <136144838+bluemarco@users.noreply.github.com>
2023-07-22 23:41:21 +0800

    Create basic NoteService methods

--- src/main/java/io/bluemarco/note/service/NoteService.java
+++ src/main/java/io/bluemarco/note/service/NoteService.java
@@ -1,7 +1,27 @@
 package io.bluemarco.note.service;
 
+import io.bluemarco.note.collection.Note;
+import io.bluemarco.note.repository.NoteRepositoryImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class NoteService {
+
+    @Autowired
+    private  NoteRepositoryImpl noteRepository;
+
+    public void addNew(Note note){
+        noteRepository.addNew(note);
+    }
+
+    public List<Note> getAll(){
+        return noteRepository.getAll();
+    }
+
+    public void delete(String ID){
+        noteRepository.deleteByID(ID);
+    }
 }
